dd2e1a163ef43380c2b5bd6449ead29ffa63b4a4
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2024-12-13 15:58:44 +0530

    Use update_deployment to update deployments

--- sunbeam-python/sunbeam/provider/local/commands.py
+++ sunbeam-python/sunbeam/provider/local/commands.py
@@ -710,7 +710,7 @@ def bootstrap(
 
         # Reload deployment with sunbeam-controller {fqdn} user credentials
         deployment.reload_credentials()
-        deployments.write()
+        deployments.update_deployment(deployment)
         deployment.reload_tfhelpers()
         jhelper = JujuHelper(deployment.get_connected_controller())
 
